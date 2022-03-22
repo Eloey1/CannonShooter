9cc830578cfac6c29e55bddcd6500b2872d30ee7
Eloey1 <emilb2002@gmail.com>
2022-03-22 12:57:38 +0100

    test

---
+++ Assets/Scenes/Emil/Scrips/Exempel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exempel : MonoBehaviour
+{
+
+    private float testExempel;
+    public int exempelTest;
+    static bool staticBool;
+    [SerializeField] Rigidbody rigidbody;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    #region DelaUppKod
+
+    void Test()
+    {
+
+    }
+
+    #endregion
+}
