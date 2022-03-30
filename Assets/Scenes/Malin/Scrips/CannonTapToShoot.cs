f7051865c3cb0fb77856671ae6ea15b89e1b21f5
mixy02 <mollie248@gmail.com>
2022-03-30 14:52:33 +0200

    Touch to Shoot

---
+++ Assets/Scenes/Malin/Scrips/CannonTapToShoot.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CannonTapToShoot : MonoBehaviour
+{
+    [Header("Shooting the Ball")]
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] float shootForce;
+    private BoxCollider2D collider;
+    [SerializeField] Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        //button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+
+            //Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            //touchPosition.z = 0f;
+
+            //transform.position = touchPosition;
+
+            //touch.phase == TouchPhase.Began
+
+            //if (touch.phase == TouchPhase.Began)
+            //{
+            //    button.onClick.AddListener(Shoot);
+            //}
+
+            if(Vector2.Distance((Vector2)transform.position, touch.position) <= 200)
+            {
+                Shoot();
+            }
+        }
+
+
+    }
+
+    void Shoot()
+    {
+        GameObject ball = Instantiate(ballPrefab, shootPoint.position, shootPoint.rotation);
+        Rigidbody2D ballRb = ball.GetComponent<Rigidbody2D>();
+        ballRb.AddForce(shootPoint.up * shootForce, ForceMode2D.Impulse);
+    }
+}
