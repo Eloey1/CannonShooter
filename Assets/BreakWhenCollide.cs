b32adea835b62a30178bebb7f7c8aec063032875
mixy02 <mollie248@gmail.com>
2022-04-19 16:06:32 +0200

    lvl4
    klar, bara partikeleffekt klar

--- Assets/BreakWhenCollide.cs
+++ Assets/BreakWhenCollide.cs
@@ -6,14 +6,31 @@ public class BreakWhenCollide : MonoBehaviour
 {
     private void Update()
     {
-        OnTriggerBreak2D(GetComponent<BoxCollider2D>());
+        //OnTriggerBreak2D(GetComponent<BoxCollider2D>());
     }
-    private void OnTriggerBreak2D(Collider2D collision)
+    //private void OnTriggerBreak2D(Collider2D collision)
+    //{
+    //    on
+
+    //    if (collision.gameObject.tag == "Ball")
+    //    {
+    //        Debug.Log("Break plank");
+    //        Destroy(this);
+    //        //goal = true;
+    //    }
+    //    if (collision.gameObject.tag == "Ball")
+    //    {
+    //        GameObject ball = GameObject.FindGameObjectWithTag("Ball");
+    //        Rigidbody2D ballRb = ball.GetComponent<Rigidbody2D>();
+    //        ballRb.AddForce(ballRb.velocity * boostForce);
+    //    }
+    //}
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ball")
         {
             Debug.Log("Break plank");
-            Destroy(this);
+            Destroy(gameObject);
             //goal = true;
         }
     }
